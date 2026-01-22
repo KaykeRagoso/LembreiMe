e15185778b6c07fc849c8d1ec8b2788a90b9b7dc
Kayke Ragoso <162835740+KaykeRagoso@users.noreply.github.com>
2026-01-22 08:29:30 -0300

    refine gitignore e firebase

--- services/firebase.js
+++ services/firebase.js
@@ -1,21 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCqggmax8W7e85BSfeK0C2Tspokh9K4GZQ",
-  authDomain: "lembreime-6df58.firebaseapp.com",
-  projectId: "lembreime-6df58",
-  storageBucket: "lembreime-6df58.firebasestorage.app",
-  messagingSenderId: "684975032955",
-  appId: "1:684975032955:web:a142503ad20c00d66730de",
-  measurementId: "G-6341SQ0M29"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
