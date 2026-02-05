75130604cf7108cf351fa189312a23dd47e066dc
Kayke Ragoso <162835740+KaykeRagoso@users.noreply.github.com>
2026-02-05 19:41:38 -0300

    Done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,35 +1,17 @@
-// app/_layout.tsx
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
     <>
-      <StatusBar style="dark"/>
+      <StatusBar style="dark" />
       <Stack
-        screenOptions={{
-          headerShown: false,
-          animation: 'slide_from_right',
-        }}
+        initialRouteName="login" 
+        screenOptions={{ headerShown: false }}
       >
-        {/* Tela inicial - lista de tarefas */}
-        <Stack.Screen name="index" />
-        
-        {/* Tela de criar tarefa */}
-        <Stack.Screen 
-          name="create" 
-          options={{
-            animation: 'slide_from_bottom',
-          }}
-        />
-        
-        {/* Tela de editar tarefa (parâmetro dinâmico) */}
-        <Stack.Screen 
-          name="edit/[id]" 
-          options={{
-            animation: 'slide_from_bottom',
-          }}
-        />
+        <Stack.Screen name="login" />
+        <Stack.Screen name="register" />
+        <Stack.Screen name="reset-password" />
       </Stack>
     </>
   );
